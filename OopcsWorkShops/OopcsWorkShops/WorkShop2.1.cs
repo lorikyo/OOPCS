ab802970641084af1cf6de44ec24b1fee7afa5ad
lorikyo <davidfan09@yahoo.com>
2016-09-23 15:33:55 +0800

    workshop2.1

---
+++ OopcsWorkShops/OopcsWorkShops/WorkShop2.1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISS.RV.LIB;
+
+namespace OopcsWorkShops
+{
+    public class Coin
+    {
+        // Attributes
+        private int face;
+
+        // Methods
+        public int GetFace()
+        {
+            return face;
+        }
+
+        public void Flip()
+        {
+            face = ISSMath.RNDInt(2);
+        }
+
+        // Constructors
+        public Coin()
+        {
+            Flip();
+        }
+
+        // Properties
+        public string StrFace
+        {
+            get
+            {
+                if (GetFace() == 0) return "HEAD";
+                else return "TAIL";
+            }
+        }
+    }
+
+    public class WorkShop2
+    {
+        public static void Main()
+        {
+            Coin coin1 = new Coin();
+            coin1.Flip();
+            coin1.Flip(); Console.WriteLine("Coin1: {0}", coin1.StrFace);
+            coin1.Flip(); Console.WriteLine("Coin1: {0}", coin1.StrFace);
+            coin1.Flip(); Console.WriteLine("Coin1: {0}", coin1.StrFace);
+
+            Coin coin2 = new Coin();
+            coin2.Flip(); Console.WriteLine("Coin2: {0}", coin2.StrFace);
+
+            Console.WriteLine("Coin1: {0}", coin1.StrFace);
+        }
+    }
+}
